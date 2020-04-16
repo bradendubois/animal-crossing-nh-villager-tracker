1580cd1b22c34b49402c4b36596208ba73e2a0f7
Braden <drab3n@gmail.com>
2020-04-16 12:27:15 -0600

    Added villager data, importing stylesheets works

---
+++ unique_attributes.py
@@ -0,0 +1,13 @@
+import json
+
+with open('./villager-data/villager-data.json', 'r') as f:
+    villager_data = json.load(f)
+
+
+unique_keys = []
+for villager in villager_data:
+    for key in villager_data[villager]:
+        if key not in unique_keys:
+            unique_keys.append(key)
+
+print(unique_keys)
