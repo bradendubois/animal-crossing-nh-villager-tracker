cfdcf45403f8b9110ae1d138dd7935e228e4b0e2
Braden <drab3n@gmail.com>
2020-04-15 18:00:58 -0600

    Inline styling works for selected attribute

--- assets/imports.js
+++ assets/imports.js
@@ -1,9 +1,10 @@
 // All imported files in the document
 const links = document.querySelectorAll('link[rel="import"]')
 
+
 // Import and add each page to the DOM
 Array.prototype.forEach.call(links, (link) => {
-  let template = link.import.querySelector('.entry')
-  let clone = document.importNode(template.content, true)
-  document.querySelector('.content').appendChild(clone)
+    let template = link.import.querySelector('.entry')
+    let clone = document.importNode(template.content, true)
+    document.querySelector('.content').appendChild(clone)
 })
