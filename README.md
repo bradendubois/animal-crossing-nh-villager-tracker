960d3ee1a74f936162226ad66bb9d57e684e19ef
Braden <drab3n@gmail.com>
2020-04-19 15:12:02 -0600

    Main.js cleanup, updated ESlint, added README badges

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-animal-crossing-nh-villager-tracker
+# animal-crossing-nh-villager-tracker
+
+![HTML](https://img.shields.io/badge/html-5-red?style=for-the-badge)
+[![SCSS](https://img.shields.io/badge/scss-1.3.8-mediumgreen?style=for-the-badge)](https://sass-lang.com/)
+[![JavaScript](https://img.shields.io/badge/javascript-yellow?style=for-the-badge)](https://www.javascript.com/)
+[![React](https://img.shields.io/badge/react-9feaf9?style=for-the-badge)](https://www.electronjs.org/)
+
+![Last Commit](https://img.shields.io/github/last-commit/bradendubois/animal-crossing-nh-villager-tracker?style=for-the-badge)
+
--- main.js
+++ main.js
@@ -8,87 +8,80 @@ const {app, BrowserWindow} = require('electron')
 
 const debug = /--debug/.test(process.argv[2])
 
-if (process.mas) app.setName('Electron APIs')
-
-let mainWindow = null
+let mainWindow = null;
   
 function initialize () {
-    makeSingleInstance()
-  
-    //loadDemos()
-  
-    function createWindow () {
-      const windowOptions = {
-        width: 1280,
-        height: 840,
-        
-        "minWidth": 800,
-        "minHeight": 600,
+  makeSingleInstance()
 
-        title: app.getName(),
-        webPreferences: {
-          nodeIntegration: true
-        }
-      }
-  
-      if (process.platform === 'linux') {
-        windowOptions.icon = path.join(__dirname, '/assets/app-icon/png/512.png')
-      }
-  
-      mainWindow = new BrowserWindow(windowOptions)
-      mainWindow.loadURL(path.join('file://', __dirname, '/content/index.html'))
+  //loadDemos()
+
+  function createWindow () {
+    
+    const windowOptions = {
+      width: 1280,
+      height: 840,
       
-      if (debug) {
-        mainWindow.webContents.openDevTools()
+      "minWidth": 800,
+      "minHeight": 600,
+
+      title: app.getName(),
+      webPreferences: {
+        nodeIntegration: true
       }
-  
-      mainWindow.on('closed', () => {
-        mainWindow = null
-      })
     }
-  
-    app.on('ready', () => {
+
+    if (process.platform === 'linux') {
+      windowOptions.icon = path.join(__dirname, '/assets/app-icon/png/512.png')
+    }
+
+    mainWindow = new BrowserWindow(windowOptions)
+    mainWindow.loadURL(path.join('file://', __dirname, '/content/index.html'))
+    
+    if (debug) {
+      mainWindow.webContents.openDevTools()
+    }
+
+    mainWindow.on('closed', () => {
+      mainWindow = null
+    })
+  }
+
+  app.on('ready', () => {
+    createWindow()
+  })
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+
+  app.on('activate', () => {
+    if (mainWindow === null) {
       createWindow()
-    })
+    }
+  })
+}
   
-    app.on('window-all-closed', () => {
-      if (process.platform !== 'darwin') {
-        app.quit()
-      }
-    })
-  
-    app.on('activate', () => {
-      if (mainWindow === null) {
-        createWindow()
-      }
-    })
-  }
-  
-  // Make this app a single instance app.
-  //
-  // The main window will be restored and focused instead of a second window
-  // opened when a person attempts to launch a second instance.
-  //
-  // Returns true if the current version of the app should quit instead of
-  // launching.
-  function makeSingleInstance () {
-    if (process.mas) return
-  
-    app.requestSingleInstanceLock()
-  
-    app.on('second-instance', () => {
-      if (mainWindow) {
-        if (mainWindow.isMinimized()) mainWindow.restore()
-        mainWindow.focus()
-      }
-    })
-  }
-  
-  // Require each JS file in the main-process dir
-  function loadDemos () {
-    const files = glob.sync(path.join(__dirname, 'main-process/**/*.js'))
-    files.forEach((file) => { require(file) })
- }
-  
-  initialize()
+// Make this app a single instance app.
+//
+// The main window will be restored and focused instead of a second window
+// opened when a person attempts to launch a second instance.
+//
+// Returns true if the current version of the app should quit instead of
+// launching.
+function makeSingleInstance () {
+  if (process.mas) return
+
+  app.requestSingleInstanceLock()
+
+  app.on('second-instance', () => {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+  })
+}
+
+initialize()
   
